dca71b59d1e30d0892ca68960b61034344914e18
radhakrishnanr682-hue <radhakrishnanr682@gmail.com>
2026-01-05 19:21:08 +0530

    modified

--- qn5.py
+++ qn5.py
@@ -1,20 +1,20 @@
-# Accept basic salary
+
 basic = float(input("Enter Basic Salary: "))
 
-# Calculate HRA and DA
+
 hra = 0.20 * basic
 da = 0.10 * basic
 
-# Calculate total (gross) salary
+
 gross_salary = basic + hra + da
 
-# Calculate tax
+
 tax = 0.05 * gross_salary
 
-# Calculate net salary
+
 net_salary = gross_salary - tax
 
-# Display results
+
 print("Basic Salary:", basic)
 print("HRA (20%):", hra)
 print("DA (10%):", da)
